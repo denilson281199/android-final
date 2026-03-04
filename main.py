625c73f1b58ba51fdfcfbe264d0ed72f42853bb3
denilson281199 <denilson281199@gmail.com>
2026-03-04 12:06:34 +0930

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-import requests
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.clock import Clock
-
-URL_NGROK = "https://bleriest-portraitlike-dreama.ngrok-free.dev"
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 class SystemUpdateApp(App):
     def build(self):
-        # Label simples atu hatene app funsiona
-        return Label(text='System Update App\nRunning...')
-
-    def on_start(self):
-        # Chama funsaun kada 5 segundos
-        Clock.schedule_interval(self.send_request, 5)
-
-    def send_request(self, dt):
-        try:
-            response = requests.get(f"{URL_NGROK}/ping", timeout=5)
-            print(f"Response: {response.status_code}")
-        except Exception as e:
-            print(f"Error: {e}")
+        layout = BoxLayout(orientation='vertical')
+        label = Label(text='System Update App\nRunning...')
+        button = Button(text='Click Me')
+        layout.add_widget(label)
+        layout.add_widget(button)
+        return layout
 
 if __name__ == '__main__':
     SystemUpdateApp().run()
